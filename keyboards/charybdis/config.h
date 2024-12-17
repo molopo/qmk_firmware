7ec5ca0fc3db32eb74985453508ed25831b3d8d9
Michael Mei <michaelmei@MacBook-Air.local>
2024-12-16 22:19:19 -0500

    monday night

--- keyboards/charybdis/config.h
+++ keyboards/charybdis/config.h
@@ -26,8 +26,8 @@
 
 /* key matrix size */
 #define MATRIX_ROWS 8
-#define MATRIX_COLS 10
-
+#define MATRIX_COLS 5
+#define ROTATIONAL_TRANSFORM_ANGLE -90
 /*
  * Keyboard Matrix Assignments
  *
@@ -41,6 +41,7 @@
 #define MATRIX_COL_PINS { F4, F5, F6, F7, B1 }
 #define MATRIX_COL_PINS_RIGHT { B4, B5, F6, F4, F5 }
 
+#define MATRIX_ROW_PINS_RIGHT { D4, C6, D7, E6 }
 #define MATRIX_ROW_PINS { D4, C6, D7, E6 }
 
 // /* COL2ROW, ROW2COL*/
@@ -52,7 +53,7 @@
 #define SOFT_SERIAL_PIN D2  // or D1, D2, D3, E6
 
 #define MASTER_LEFT
-#define SPLIT_USB_DETECT
+
 /* Pointing device configuration. */
 
 
@@ -66,7 +67,8 @@
 #define POINTING_DEVICE_TASK_THROTTLE_MS 1
 
 // Invert X axis on mouse reports.
-#define POINTING_DEVICE_INVERT_X
+
+//#define POINTING_DEVICE_INVERT_X
 
 #define PMW33XX_CS_PIN F7
 # define PMW3360_CPI 400
@@ -122,10 +124,11 @@
 // #    endif // !__arm__
 // #endif
 
-#ifdef VIA_ENABLE
-/* VIA configuration. */
-#    define DYNAMIC_KEYMAP_LAYER_COUNT 7
-#endif // VIA_ENABLE
+// #ifdef VIA_ENABLE
+// /* VIA configuration. */
+// #    define DYNAMIC_KEYMAP_LAYER_COUNT 7
+// #endif // VIA_ENABLE
+
 
 /* Disable unused features. */
 #define NO_ACTION_ONESHOT
